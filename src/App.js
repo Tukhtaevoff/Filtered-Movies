eb89e18f39d4f2c34d775b65327b5194adcbfbdd
Tukhtaev27 <108573349+Tukhtaev27@users.noreply.github.com>
2023-01-25 20:49:30 +0500

    master

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import "./App.css";
+import { useEffect, useState } from "react";
+import Movie from "./Movie";
+import Filter from "./Filter";
+
+function App() {
+  const [popular, setPopular] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  const [activeGenre, setActiveGenre] = useState(0);
+
+  useEffect(() => {
+    fetchPopular();
+  }, []);
+
+  const fetchPopular = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=d5c35e51c81488b19da7c1f572507a3d&language=en-US&page=1"
+    );
+    const movies = await data.json();
+    console.log(movies);
+    setPopular(movies.results);
+    setFiltered(movies.results);
+  };
+
+  return (
+    <div className="App">
+      <Filter
+        popular={popular}
+        setFiltered={setFiltered}
+        activeGenre={activeGenre}
+        setActiveGenre={setActiveGenre}
+      />
+
+      <div className="popular-movies">
+        {filtered &&
+          filtered.map((movie) => {
+            return <Movie key={movie.id} movie={movie} />;
+          })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Movie.js
@@ -0,0 +1,15 @@
+import { motion } from 'framer-motion'
+
+function Movie({ movie }) {
+  return (
+    <motion.div layout>
+      <h2>{movie.title}</h2>
+      <img
+        src={"https://image.tmdb.org/t/p/w500" + movie.backdrop_path}
+        alt=""
+      />
+    </motion.div>
+  );
+}
+
+export default Movie;
